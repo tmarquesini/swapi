a952142b2f3a60feb2e2a5dd543eb77207728d17
Thiago Marquesini <thiagomarquesini@madeiranit.com.br>
2021-10-13 18:28:49 -0300

    Add H2 dependency and configure database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+drop table if exists swapi;
+
+create table users (
+        id int not null auto_increment,
+        username varchar(32) not null unique,
+        password varchar(255) not null,
+        token varchar(255),
+        primary key (id)
+);
+
+insert into users (username, password) values ('user', 'secret');
