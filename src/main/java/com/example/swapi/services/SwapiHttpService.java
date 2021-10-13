b59f0cb50cdb9b39abb57b09255453b2a20d9a92
Thiago Marquesini <thiagomarquesini@madeiranit.com.br>
2021-10-13 16:36:48 -0300

    Move wwapi base url to application properties

--- src/main/java/com/example/swapi/services/SwapiHttpService.java
+++ src/main/java/com/example/swapi/services/SwapiHttpService.java
@@ -1,5 +1,6 @@
 package com.example.swapi.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,12 +9,14 @@ public class SwapiHttpService {
 
     private final RestTemplate restTemplate;
 
+    @Value("${swapi.base_url}")
+    private String baseUrl;
+
     public SwapiHttpService() {
         this.restTemplate = new RestTemplate();
     }
 
     public String sendRequest(String endpoint) {
-        String baseUrl = "https://swapi.dev/api/";
         return restTemplate.getForObject(baseUrl + endpoint, String.class);
     }
 
