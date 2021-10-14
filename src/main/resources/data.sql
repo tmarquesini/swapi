08607e912bc50c50e4a72a077581e83b70372062
Thiago Marquesini <thiagomarquesini@madeiranit.com.br>
2021-10-14 01:12:02 -0300

    Remove unused field

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,5 @@ create table users (
         id int not null auto_increment,
         username varchar(32) not null unique,
         password varchar(255) not null,
-        token varchar(255),
         primary key (id)
 );
